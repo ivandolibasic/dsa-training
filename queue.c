b87692c33fb163a40b5c79b0b2c3c350815a9c1a
DESKTOP-LNCI328\Ivan <idolibasic@outlook.com>
2024-02-06 00:02:42 +0100

    Add implementations of stack and queue

---
+++ queue.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int queue[256] = {0};
+int count = 0;
+
+void enqueue(int x) {
+    queue[count] = x;
+    count++;
+}
+
+int dequeue() {
+    int result = queue[0];
+    for (int i = 0; i < count - 1; i++)
+        queue[i] = queue[i + 1];
+    count--;
+    return result;
+}
+
+int main() {
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    enqueue(4);
+
+    dequeue();
+    enqueue(5);
+
+    int length = count;
+    for (int i = 0; i < length; i++) {
+        printf("%d ", dequeue());
+    }
+    printf("\n");
+
+    return 0;
+}
